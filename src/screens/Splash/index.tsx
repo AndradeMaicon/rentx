7566d3f12106f43983c082ad0a0ed268a1cff558
Maicon <andrade.maicon.pr@gmail.com>
2021-09-13 20:39:00 -0300

    Creating splash screen

---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from 'react';
+import { CommonActions, useNavigation } from '@react-navigation/native';
+
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  interpolate,
+  runOnJS,
+} from 'react-native-reanimated';
+
+import { Container } from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { StatusBar } from 'expo-status-bar';
+
+export function Splash() {
+  const splashAnimation = useSharedValue(0);
+
+  const navigation = useNavigation();
+
+  const brandStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value,
+        [0, 25, 50, 75],
+        [1, .3, 0, 0]
+      )
+    }
+  })
+
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value,
+        [0, 25, 50, 75],
+        [0, 0, .3, 1]
+      )
+    }
+  })
+
+  function startApp() {
+    navigation.dispatch(
+      CommonActions.navigate({
+        name: 'Home'
+      })
+    )
+  }
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(
+      75,
+      { duration: 6000 },
+      () => {
+        'worklet'
+        runOnJS(startApp)();
+      }
+    )
+  }, [])
+
+  return (
+    <Container>
+      <StatusBar 
+        hidden
+      />
+
+      <Animated.View style={[{ position: 'absolute' }, brandStyle]}>
+        <BrandSvg width={RFValue(78)} height={RFValue(48)} />
+      </Animated.View>
+
+      <Animated.View style={[{ position: 'absolute' },logoStyle]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
+
+    </Container>
+  );
+}
